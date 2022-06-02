d83f6cbb222ccf2a545c94d51fa1fa8afc8ae81a
Ulises Bonilla <ulises@modular.mx>
2022-06-02 12:43:02 -0500

    inicio lucky

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>LuckyAlien</title>
+  <link rel="shortcut icon" href="logomini.png">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body style="background-color: #E8E8E8;">
+  
+<div class="container text-center">
+
+  <div class="row">
+    
+    <div class="col-md-12 col-sm-12 col-xs-12" style="padding: 13%;">
+
+      <img src="logo.png" height="500px">
+    
+    </div>
+
+  </div>
+
+</div>
+
+</body>
+</html>
